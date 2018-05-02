cab661ad726ad6b8f1ad15e17b0864c299d63ba8
Micah Chiang <micahkchiang@gmail.com>
2018-05-02 13:20:12 -0700

    todo app

---
+++ todo-app/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <todo-list :todos="todos"></todo-list>
+    <create-todo @create-todo="addTodo"></create-todo>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    CreateTodo
+  },
+  data() {
+    return {
+      todos: [
+        {
+          title: 'Todo A',
+          project: 'Project A',
+          done: false
+        },
+        {
+          title: 'Todo B',
+          project: 'Project B',
+          done: false
+        },
+        {
+          title: 'Todo C',
+          project: 'Project C',
+          done: false
+        },
+        {
+          title: 'Todo D',
+          project: 'Project D',
+          done: false
+        }
+      ]
+    };
+  },
+  methods: {
+    addTodo(todo) {
+      this.todos.push(todo);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ todo-app/src/components/TodoList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+        <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+        <todo @complete-todo="completeTodo" @delete-todo="deleteTodo" v-for="(todo, index) in todos" :todo="todo" :key="index"></todo>
+    </div>
+</template>
+
+<script>
+import Todo from './Todo';
+export default {
+  props: ['todos'],
+  components: {
+    Todo
+  },
+  methods: {
+    deleteTodo(todo) {
+      let todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+    completeTodo(todo) {
+      let todoIndex = this.todos.indexOf(todo);
+      this.todos[todoIndex].done = true;
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
+
+

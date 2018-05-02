d8286a11f82f3df10366c0b041c7f331871ea625
Micah Chiang <micahkchiang@gmail.com>
2018-05-01 17:26:14 -0700

    vue-skills

---
+++ vue-skills/src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+      <h1>Hello {{ their_name }}, this is an about page</h1>
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Sunt esse, dicta quae cum deleniti minima expedita labore animi soluta. 
+            In praesentium nostrum nulla impedit, 
+            quisquam non mollitia tenetur repellendus harum!
+        </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      their_name: this.$route.params.name
+    };
+  }
+};
+</script>

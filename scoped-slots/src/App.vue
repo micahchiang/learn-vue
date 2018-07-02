c4783ac0a8cc5bdbb96ccdf8a5ffcd81a553b648
Micah Chiang <micahkchiang@gmail.com>
2018-07-02 15:29:50 -0700

    scoped slots

---
+++ scoped-slots/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <slot-component>
+      <template slot-scope="{contact}">
+        {{contact.name}}
+      </template>
+    </slot-component>
+  </div>
+</template>
+
+<script>
+import SlotComponent from './slot.component.vue';
+
+export default {
+  name: 'app',
+  components: {
+    SlotComponent
+  },
+  data() {
+    return {
+      msg: 'Scoped Slots: A Study'
+    };
+  }
+};
+</script>
+
+<style scoped>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  width: 200px;
+  height: 200px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ scoped-slots/src/slot.component.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="SlotComponent">
+        <h1>{{msg}}</h1>
+        <div class="contact__container">
+            <div v-for="contact in contacts" :key="contact.id" class="contact__container-item">
+                <slot :contact="contact"></slot>
+                <h4>{{contact.email}}</h4>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SlotComponent',
+  data() {
+    return {
+      msg: 'hello slots',
+      contacts: []
+    };
+  },
+  created() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(contacts => (this.contacts = contacts));
+  }
+};
+</script>
+
+<style scoped>
+h1,
+h3,
+h4 {
+  font-weight: 400;
+  margin: 0;
+}
+
+.contact__container-item {
+  padding: 1em;
+}
+</style>
+
+

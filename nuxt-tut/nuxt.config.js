e12b05d90ff0b7b51086ab62d368c953c0012844
Micah Chiang <micahkchiang@gmail.com>
2018-08-07 18:02:55 -0700

    nuxt

---
+++ nuxt-tut/nuxt.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'nuxt-tut',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  css: [
+    'tachyons/css/tachyons.min.css',
+    'tachyons-debug/css/tachyons-debug.min.css'
+  ],
+  build: {
+    vendor: ['axios']
+  }
+};
